fb095af9090f5014a06545aeae3808b731e89096
moreschini16 <61256035+moreschini16@users.noreply.github.com>
2020-10-02 10:42:58 +0300

    add comments on LogisticRegression

--- Classification_scripts/config.py
+++ Classification_scripts/config.py
@@ -25,7 +25,7 @@ randstate = 0 #define random state for models initialization
 
 # 1) Logistic Regression
 #    penalty for LR
-LRPenalty = "l2" #NO l1!
+LRPenalty = "l2" #no L1
 
 
 # 2) Random Forest
